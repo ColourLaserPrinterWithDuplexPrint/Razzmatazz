c544a711015fbf773b4a78ff5d4baa3684f5cb88
ColourLaserPrinterWithDuplexPrint <79779415+ColourLaserPrinterWithDuplexPrint@users.noreply.github.com>
2021-03-06 16:36:14 +1100

    Comments

--- main/Razzmatazz.py
+++ main/Razzmatazz.py
@@ -2,27 +2,41 @@ if __name__ == '__main__':
     try:
         file = input("Image To Distort : ")
         file2 = input("Output File (Creates A New File) : ")
+        #Taking Input
+        
         repl1 = input("Character To Replace : ")
         repl2 = input("Character To Replace With : ")
+        #Gives the user an option of how to distort the image
+        
         with open(file, 'rb') as fileHandler:
+            #Opens the file in read bytes mode
+            
             contents = fileHandler.read()
+            #Gets the contents of the file
+            
             contents = contents.decode('iso-8859-1')
+            #Decodes the contents of the file with the 'iso-8859-1' format
+            
             contents = contents.replace(repl1, repl2)
+            #Replaces the first input character with the second input character
+            
         with open(file2, 'x') as fileHandler:
+            #Creates a new file
+            
             print("Created New File")
+            
             contents = contents.encode('iso-8859-1')
+            #Re-Encodes the file contents
+            
         with open(file2, 'wb') as fileHandler:
+            #Opens the created output file
+            
             fileHandler.write(contents)
+            #Writes the contents to the output file
+            
         input("Done ! : ")
     except Exception as e:
+        #Checks if there's an error
+        
         print(e)
         input("Press Any Key To Exit : ")
-
-def distortImage(file, output):
-    with open(file, 'rb') as fileHandler:
-        contents = fileHandler.read()
-        contents = contents.decode('iso-8858-1')
-        contents = contents.replace('t', 'y')
-        contents = contents.encode('iso-8858-1')
-    with open(file, 'wb') as fileHandler:
-        fileHandler.write(contents)
