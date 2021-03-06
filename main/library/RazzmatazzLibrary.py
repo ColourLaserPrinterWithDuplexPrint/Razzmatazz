a436e96abe982d7b57f3294efbaca5130c7acc1b
ColourLaserPrinterWithDuplexPrint <79779415+ColourLaserPrinterWithDuplexPrint@users.noreply.github.com>
2021-03-06 16:43:54 +1100

    Update RazzmatazzLibrary.py

--- main/library/RazzmatazzLibrary.py
+++ main/library/RazzmatazzLibrary.py
@@ -1,14 +1,22 @@
 import random
-def distortImage(file, output):
+def distortImage(file, output, char1, char2):
+    #Usage : distortImage('image.jpg', 'glitchedImage.jpg', 't', 'y')
     with open(file, 'rb') as fileHandler:
         contents = fileHandler.read()
         contents = contents.decode('iso-8858-1')
-        contents = contents.replace('t', 'y')
+        contents = contents.replace(char1, char2)
         contents = contents.encode('iso-8858-1')
     try:
         fileHandler = open(output, 'x')
     except:
          pass
-   
    with open(output, 'wb') as fileHandler:
         fileHandler.write(contents)
+ 
+def getDistortedContents(file, char1, char2):
+    with open(file, 'rb') as fileHandler:
+        contents = fileHandler.read()
+        contents = contents.decode("iso-8858-1")
+        contents = contents.replace(char1, char2)
+        contents = contents.encode("iso-8858-1")
+        return contents
